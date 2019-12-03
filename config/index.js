34884fc11a9e70c5a259886cfbba6099597552dc
liaokui <liaokui@duc.cn>
2019-12-03 19:03:55 +0800

    1) 包;

--- config/index.js
+++ config/index.js
@@ -56,7 +56,7 @@ module.exports = {
     index: path.resolve(__dirname, '../dist/index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsRoot: path.resolve(__dirname, './dist'),
     assetsSubDirectory: 'static',
 
     /**
